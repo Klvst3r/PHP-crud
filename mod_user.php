97f5af2b1aca903c17b7337694a39c2a6292b9f4
Klvst3r <klvst3r@gmail.com>
2017-01-30 19:53:59 -0600

    Modify User in DataBase

---
+++ mod_user.php
@@ -0,0 +1,45 @@
+<?php 
+	
+	require('sql/connection.php');
+	
+	$id = $_POST['id'];
+	$usuario = $_POST['usuario'];
+	$password = $_POST['password'];
+	$email = $_POST['email'];
+	
+	$query = "UPDATE usuarios SET usuario='$usuario', contrasenia='$password', email='$email' WHERE id='$id'";
+	
+	$resultado = $mysqli->query($query);
+	
+?>
+
+<html>
+	<head>
+		<title>PHP-CRUD</title>
+	</head>
+	
+	<body>
+	<h1>Modificar usuario</h1>
+		<center>
+			
+			<?php 
+				if($resultado>0){
+				?>
+				
+				<h1>Usuario Modificado</h1>
+				
+					<?php 	}else{ ?>
+				
+				<h1>Error al Modificar Usuario</h1>
+				
+			<?php	} ?>
+			
+			<p></p>	
+			
+			<a href="index.php">Regresar</a>
+			
+		</center>
+	</body>
+</html>
+				
+				
